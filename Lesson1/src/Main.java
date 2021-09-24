bf5f474d146a1028bf4f73e1c65f86b0bbb0fca9
12532 <yylm1111@gmail.com>
2021-09-23 20:05:04 -0700

    Submit Class one note

--- Lesson1/src/Main.java
+++ Lesson1/src/Main.java
@@ -3,5 +3,53 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+        int numOfStudentsInClass = 62;
+        Integer test = new Integer(52);
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+
+        System.out.println(max);
+        System.out.println(min);
+
+        System.out.println(maxShort);
+        System.out.println(minShort);
+
+        float maxFloat = Float.MAX_VALUE;
+        float myFloat = 5.23f;
+        float minFloat = Float.MIN_VALUE;
+
+        System.out.println(maxFloat);
+        System.out.println(minFloat);
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        System.out.println(maxDouble);
+        System.out.println(minDouble);
+
+        char ch = 'A';
+        char cha = '\u0041'; //unicode
+
+        System.out.println(ch);
+        System.out.println(cha);
+
+        boolean bTestValue = true;
+        bTestValue = !bTestValue;
+        System.out.println(bTestValue);
+
+        long myLong = 5;
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        System.out.println(maxLong);
+        System.out.println(minLong);
+
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+        System.out.println(maxByte);
+        System.out.println(minByte);
     }
 }
