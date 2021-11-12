a4b092c388553ac1d7040aefa7c931f0109747ff
12532 <yylm1111@gmail.com>
2021-11-12 15:59:14 -0800

    commit submitted

--- Homework4/src/LinkedList.java
+++ Homework4/src/LinkedList.java
@@ -13,6 +13,75 @@ public class LinkedList {
         }
     }
 
+    public LinkedList(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void addAtPosition(int index, int element){
+        synchronized (Main.obj){
+            Node newNode = new Node(element);
+            Node currNode = head, prevNode = null;
+            int position = 0;
+            if(this.head == null){
+                this.head = newNode;
+                size ++;
+                return;
+            }
+            else if(index == 0){
+                head = newNode;
+                newNode.next = currNode;
+                size ++;
+                return;
+            }
+
+            if(index > this.size() || index < 0) return;
+
+            while(position < index - 1){
+                currNode = currNode.next;
+                position ++;
+            }
+
+            if(position == index - 1){
+                newNode.next = currNode.next;
+                currNode.next = newNode;
+                size ++;
+            }
+
+
+        }
+    }
+
+    public void removeAtPosition(int index, int element){
+        synchronized (Main.obj){
+            if(index > size() - 1 || index < 0) return;
+
+            Node currNode = head, prevNode = null;
+            int position = 0;
+
+            if(index == 0 && head.val == element){
+                this.head = currNode.next;
+                size --;
+                return;
+            }
+
+            while(position != index){
+                prevNode =currNode;
+                currNode = currNode.next;
+                position ++;
+            }
+            if(currNode.val == element){
+                prevNode.next = currNode.next;
+                size --;
+                return;
+            }
+            else
+                System.out.println("The element in this position is not matched.");
+
+
+        }
+    }
+
     public int getFirst(){
         synchronized (Main.obj){
             return head.val;
@@ -31,11 +100,7 @@ public class LinkedList {
     }
 
     public int size(){
-        synchronized (Main.obj){
-            size = 0;
-            for(Node n = head; n.next != null; n = n.next)
-                size ++;
-
+        synchronized (Main.obj) {
             return size;
         }
     }
