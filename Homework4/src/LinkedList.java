c96d6785459075a76f9f06e5e92df3763320808e
12532 <yylm1111@gmail.com>
2021-11-12 11:18:54 -0800

    commit submitted

--- Homework4/src/LinkedList.java
+++ Homework4/src/LinkedList.java
@@ -1,2 +1,42 @@
 public class LinkedList {
+
+    Node head;
+    int size;
+
+    static class Node {
+        int val;
+        Node next;
+
+        Node(int val){
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public int getFirst(){
+        synchronized (Main.obj){
+            return head.val;
+        }
+    }
+
+    public int getLast(){
+        synchronized (Main.obj){
+            Node tail = head;
+            while(tail.next != null){
+                tail = tail.next;
+            }
+
+            return tail.val;
+        }
+    }
+
+    public int size(){
+        synchronized (Main.obj){
+            size = 0;
+            for(Node n = head; n.next != null; n = n.next)
+                size ++;
+
+            return size;
+        }
+    }
 }
